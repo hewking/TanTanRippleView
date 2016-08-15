4cf311b596a91653668e320c5b8abe5e4791ff10
hewking <604910070@qq.com>
2016-08-15 09:58:34 +0800

    update  starview

--- app/src/main/java/hewking/github/customviewdemo/com/hewking/widget/StarView.java
+++ app/src/main/java/hewking/github/customviewdemo/com/hewking/widget/StarView.java
@@ -2,6 +2,10 @@ package hewking.github.customviewdemo.com.hewking.widget;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -12,6 +16,8 @@ public class StarView extends View {
 
     private int mHeight;
     private int mWidth;
+
+    private Paint mPaint;
 
     public StarView(Context context) {
         this(context,null);
@@ -27,7 +33,11 @@ public class StarView extends View {
     }
 
     private void init() {
-
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStrokeWidth(2);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setAntiAlias(true);
     }
 
     @Override
@@ -48,7 +58,17 @@ public class StarView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
 
-
+        RectF rectF = new RectF(0, 0, mWidth, mHeight);
+        Path path = new Path();
+        canvas.drawRect(rectF,mPaint);
+        path.moveTo(rectF.centerX(),rectF.centerY());
+        for (int i =0 ; i < 5 ; i ++){
+            canvas.rotate(72,rectF.centerX(),rectF.centerY());
+            canvas.drawCircle(rectF.centerX() + 100,rectF.centerY() + 100,1,mPaint);
+            canvas.drawLine(rectF.centerX() + 30,rectF.centerY(),rectF.centerX() + 100,rectF.centerY() + 100,mPaint);
+            canvas.drawLine(rectF.centerX(),rectF.centerY()+ 30,rectF.centerX() + 100,rectF.centerY() + 100,mPaint);
+        }
+        canvas.rotate(- 72 * 5,rectF.centerX(),rectF.centerY());
 
         super.onDraw(canvas);
     }
