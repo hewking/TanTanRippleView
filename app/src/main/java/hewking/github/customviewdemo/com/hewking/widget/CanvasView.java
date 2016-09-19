f3652d82c57bf3cdd2d4352ca7b5b184423bbaf0
hewking <604910070qq.com>
2016-09-20 00:11:40 +0800

    extend view

--- app/src/main/java/hewking/github/customviewdemo/com/hewking/widget/CanvasView.java
+++ app/src/main/java/hewking/github/customviewdemo/com/hewking/widget/CanvasView.java
@@ -1,7 +1,22 @@
 package hewking.github.customviewdemo.com.hewking.widget;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
 /**
  * Created by hewking on 2016/9/19.
  */
-public class CanvasView {
+public class CanvasView extends View {
+    public CanvasView(Context context) {
+        super(context);
+    }
+
+    public CanvasView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CanvasView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 }
